48aab2ea30e2b23fd026bbde3ccf74d4380e6b5d
Alessandro Miliucci <lifeisfoo@gmail.com>
2023-05-19 21:37:04 +0200

    http first

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./AppSocket.jsx";
+import App from "./AppHttp.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
